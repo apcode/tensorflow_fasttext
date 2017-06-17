8427fecc9996b280f317c30efbaca76ecfe9097a
Alan Patterson <alan.patterson5@gmail.com>
2017-06-17 16:11:57 +0100

    initial commit with input processing - untested

---
+++ README.md
@@ -0,0 +1,49 @@
+# FastText in Tensorflow
+
+This is a version of Facebook's [FastText](https://github.com/facebookresearch/fastText) implemented in
+Tensorflow. However, it is not an exact replica of fastText. Instead I
+implement the main classifier using either a skipgram or cbow
+method. However I do not intend to implement heirarchical softmax,
+instead using nce loss for training.
+
+## Implemented:
+- skipgram and cbow word and sentence vectors
+- out of vocab word vectors 
+- classification of text
+- char ngrams, hashed to vectors (not yet implemented)
+- train, test and predict programs
+
+## Not Implemented:
+- heirarchical softmax (I'm using NCE (sampled) loss instead).
+- quantize models
+
+# Usage
+
+The following are examples of how to use the applications. Get full help with
+`--help` option on any of the programs.
+
+To transform input data into tensorflow Example format, an example:
+
+    process_input.py --facebook_input=queries.txt --output_dir=.
+
+Or, using a text file with one example per line with an extra file for labels:
+
+    process_input.py --text_input=queries.txt --labels=labels.txt --output_dir=.
+
+To train a new skipgram word vector model:
+
+    train_vectors.py --skipgram --input=queries.tfrecords --output_dir=model
+    
+To train a text classifier:
+
+    train_classifier.py --input=queries.tfrecords --output_dir=model
+    
+# Facebook Examples
+
+You can compare with Facebook's fastText by running similar examples
+to what's provided in their repository.
+
+    ./classification_example.sh
+    ./classification_results.sh
+    ./word_vector_example.sh
+
---
+++ process_input.py
@@ -0,0 +1,112 @@
+"""Process input data into tensorflow examples, to ease training.
+
+Input data is in one of two formats:
+- facebook's format used in their fastText library.
+- two text files, one with input text per line, the other a label per line.
+"""
+import os.path
+import re
+import sys
+import tensorflow as tf
+from itertools import izip
+from nltk.tokenize import word_tokenize
+
+
+tf.flags.DEFINE_string("facebook_input", None,
+                       "Input file in facebook train|test format")
+tf.flags.DEFINE_string("text_input", None,
+                       """Input text file containing one text phrase per line.
+                       Must have --labels defined""")
+tf.flags.DEFINE_string("labels", None,
+                       """Input text file containing one label for
+                       classification  per line.
+                       Must have --text_input defined.""")
+tf.flags.DEFINE_string("output_dir", ".",
+                       "Directory to store resulting vector models and checkpoints in")
+tf.flags.DEFINE_integer("num_shards", 1,
+                        "Number of outputfiles to create")
+FLAGS = tf.FLAGS
+
+
+def CleanText(text):
+    return word_tokenise(text.lower())
+
+
+def ParseFacebookInput(inputfile):
+    examples = []
+    for line in codecs.open(inputfile, encoding="utf-8"):
+        words = line.split()
+        # label is first field with __label__ removed
+        match = re.match(r'__label__([0-9]+)', words[0])
+        label = int(match.group(1)) if match else None
+        # Strip out label and first ,
+        words = words[2:]
+        examples.append({
+            "text": words,
+            "label": label
+        })
+    return examples
+
+
+def ParseTextInput(textfile, labelsfie):
+    examples = []
+    with codecs.open(textfile, encoding="utf-8") as f1, open(labelsfile) as f2:
+        for text, label in izip(f1, f2):
+            examples.append({
+                "text": CleanText(text),
+                "label": int(label),
+            })
+    return examples
+
+
+def WriteExamples(examples, outputfile, num_shards):
+    """Write examles in TFRecord format.
+    Args:
+      examples: list of feature dicts.
+                {'text': [words], 'label': [labels]}
+      outputfile: full pathname of output file
+    """
+    shard = 0
+    num_per_shard = len(examples) / num_shards + 1
+    for n, example in enumerate(examples):
+        if n % num_per_shard == 0:
+            shard += 1
+            writer = tf.python_io.TFRecordWriter(outputfile + '-%d-of-%d' % \
+                                                 (shard, num_shards))
+        record = tf.train.Example()
+        record["text"].bytes_list.value.extend(example["text"])
+        record["label"].int64_list.value.append(example["label"])
+        if "ngrams" in example:
+            records["ngrams"].byes_list.value.extend(example["ngrams"])
+        writer.write(record.SerializeToString())
+
+
+def WriteVocab(examples, vocabfile):
+    words = set()
+    for example in examples:
+        words.update(example["text"])
+    with codecs.open(vocabfile, encoding="utf-8", "w") as f:
+        words = sorted(list(words))
+        for word in words:
+            print >>f, word
+
+
+def main(_):
+    # Check flags
+    if not (facebook_input or (FLAGS.text_input and FLAGS.labels)):
+        print >>sys.stderr, \
+            "Error: You must define either facebook_input or both text_input and labels"
+        sys.exit(1)
+    if FLAGS.facebook_input:
+        inputfile = FLAGS.facebook_input
+        examples = ParseFacebookInput(FLAGS.facebook_input)
+    else:
+        inputfile = FLAGS.text_input
+        examples = ParseTextInput(FLAGS.text_input, FLAGS.labels)
+    fn, ext = os.path.splitext(inputfile)
+    outputfile = os.path.join(FLAGS.output_dir, fn + ".tfrecords")
+    WriteExamples(examples, outputfile, FLAGS.num_shards)
+
+
+if __name__ == '__main__':
+    tf.app.run()
